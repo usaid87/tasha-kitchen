4c472fae3243e1fc5145927eaa4cfb9f94f3744b
tasha-kitchen <hafizusaid87@gmail.com>
2025-10-28 00:22:33 +0500

    first commit

---
+++ cookware.js
@@ -0,0 +1,387 @@
+
+// row 1
+
+// p-1
+var mainImage =document.getElementById("mainImg");
+var smallimg=document.getElementsByClassName("small-img");
+
+smallimg[0].onclick=function(){
+    mainImage.src=smallimg[0].src;
+}
+
+smallimg[1].onclick=function(){
+    mainImage.src=smallimg[1].src;
+}
+
+smallimg[2].onclick=function(){
+    mainImage.src=smallimg[2].src;
+}
+
+// p-2
+var mainImage1 =document.getElementById("mainImage1");
+var smallimg1=document.getElementsByClassName("small-img1");
+
+smallimg1[0].onclick=function(){
+    mainImage1.src=smallimg1[0].src;
+}
+
+smallimg1[1].onclick=function(){
+    mainImage1.src=smallimg1[1].src;
+}
+
+smallimg1[2].onclick=function(){
+    mainImage1.src=smallimg1[2].src;
+}
+
+// p-3
+var mainImage2 =document.getElementById("mainImage2");
+var smallimg2=document.getElementsByClassName("small-img2");
+
+smallimg2[0].onclick=function(){
+    mainImage2.src=smallimg2[0].src;
+}
+
+smallimg2[1].onclick=function(){
+    mainImage2.src=smallimg2[1].src;
+}
+
+smallimg2[2].onclick=function(){
+    mainImage2.src=smallimg2[2].src;
+}
+
+// row-3
+
+// p-1
+var mainImage3 =document.getElementById("mainImage3");
+var smallimg3=document.getElementsByClassName("small-img3");
+
+smallimg3[0].onclick=function(){
+    mainImage3.src=smallimg3[0].src;
+}
+
+smallimg3[1].onclick=function(){
+    mainImage3.src=smallimg3[1].src;
+}
+
+smallimg3[2].onclick=function(){
+    mainImage3.src=smallimg3[2].src;
+}
+
+// p-2
+var mainImage4=document.getElementById("mainImage4");
+var smallimg4=document.getElementsByClassName("small-img4");
+
+smallimg4[0].onclick=function(){
+    mainImage4.src=smallimg4[0].src;
+}
+
+smallimg4[1].onclick=function(){
+    mainImage4.src=smallimg4[1].src;
+}
+
+smallimg4[2].onclick=function(){
+    mainImage4.src=smallimg4[2].src;
+}
+
+// p-3
+var mainImage5=document.getElementById("mainImage5");
+var smallimg5=document.getElementsByClassName("small-img5");
+
+smallimg5[0].onclick=function(){
+    mainImage5.src=smallimg5[0].src;
+}
+
+smallimg5[1].onclick=function(){
+    mainImage5.src=smallimg5[1].src;
+}
+
+smallimg5[2].onclick=function(){
+    mainImage5.src=smallimg5[2].src;
+}
+
+// 5th row
+// p-1
+var mainImage6=document.getElementById("mainImage6");
+var smallimg6=document.getElementsByClassName("small-img6");
+
+smallimg6[0].onclick=function(){
+    mainImage6.src=smallimg6[0].src;
+}
+
+smallimg6[1].onclick=function(){
+    mainImage6.src=smallimg6[1].src;
+}
+
+smallimg6[2].onclick=function(){
+    mainImage6.src=smallimg6[2].src;
+}
+
+// p-2
+var mainImage7=document.getElementById("mainImage7");
+var smallimg7=document.getElementsByClassName("small-img7");
+
+smallimg7[0].onclick=function(){
+    mainImage7.src=smallimg7[0].src;
+}
+
+smallimg7[1].onclick=function(){
+    mainImage7.src=smallimg7[1].src;
+}
+
+smallimg7[2].onclick=function(){
+    mainImage7.src=smallimg7[2].src;
+}
+smallimg7[3].onclick=function(){
+    mainImage7.src=smallimg7[3].src;
+}
+
+smallimg7[4].onclick=function(){
+    mainImage7.src=smallimg7[4].src;
+}
+
+smallimg7[5].onclick=function(){
+    mainImage7.src=smallimg7[5].src;
+}
+
+
+// 6th row
+// p-1
+var mainImage9=document.getElementById("mainImage9");
+var smallimg9=document.getElementsByClassName("small-img9");
+
+smallimg9[0].onclick=function(){
+    mainImage9.src=smallimg9[0].src;
+}
+
+smallimg9[1].onclick=function(){
+    mainImage9.src=smallimg9[1].src;
+}
+
+smallimg9[2].onclick=function(){
+    mainImage9.src=smallimg9[2].src;
+}
+
+// p-2
+var mainImage10=document.getElementById("mainImage10");
+var smallimg10=document.getElementsByClassName("small-img10");
+
+smallimg10[0].onclick=function(){
+    mainImage10.src=smallimg10[0].src;
+}
+
+smallimg10[1].onclick=function(){
+    mainImage10.src=smallimg10[1].src;
+}
+
+smallimg10[2].onclick=function(){
+    mainImage10.src=smallimg10[2].src;
+}
+
+// p-3
+var mainImage11=document.getElementById("mainImage11");
+var smallimg11=document.getElementsByClassName("small-img11");
+
+smallimg11[0].onclick=function(){
+    mainImage11.src=smallimg11[0].src;
+}
+
+smallimg11[1].onclick=function(){
+    mainImage11.src=smallimg11[1].src;
+}
+
+smallimg11[2].onclick=function(){
+    mainImage11.src=smallimg11[2].src;
+}
+
+// 7th row
+// p-1
+var mainImage12=document.getElementById("mainImage12");
+var smallimg12=document.getElementsByClassName("small-img12");
+
+smallimg12[0].onclick=function(){
+    mainImage12.src=smallimg12[0].src;
+}
+
+smallimg12[1].onclick=function(){
+    mainImage12.src=smallimg12[1].src;
+}
+
+smallimg12[2].onclick=function(){
+    mainImage12.src=smallimg12[2].src;
+}
+
+// p-2
+var mainImage13=document.getElementById("mainImage13");
+var smallimg13=document.getElementsByClassName("small-img13");
+
+smallimg13[0].onclick=function(){
+    mainImage13.src=smallimg13[0].src;
+}
+
+smallimg13[1].onclick=function(){
+    mainImage13.src=smallimg13[1].src;
+}
+
+smallimg13[2].onclick=function(){
+    mainImage13.src=smallimg13[2].src;
+}
+
+// 8th row
+// p-1
+var mainImage14=document.getElementById("mainImage14");
+var smallimg14=document.getElementsByClassName("small-img14");
+
+smallimg14[0].onclick=function(){
+    mainImage14.src=smallimg14[0].src;
+}
+
+smallimg14[1].onclick=function(){
+    mainImage14.src=smallimg14[1].src;
+}
+
+smallimg14[2].onclick=function(){
+    mainImage14.src=smallimg14[2].src;
+}
+
+// p-2
+var mainImage15=document.getElementById("mainImage15");
+var smallimg15=document.getElementsByClassName("small-img15");
+
+smallimg15[0].onclick=function(){
+    mainImage15.src=smallimg15[0].src;
+}
+
+smallimg15[1].onclick=function(){
+    mainImage15.src=smallimg15[1].src;
+}
+
+smallimg15[2].onclick=function(){
+    mainImage15.src=smallimg15[2].src;
+}
+
+// 9th row
+// p-1
+var mainImage16=document.getElementById("mainImage16");
+var smallimg16=document.getElementsByClassName("small-img16");
+
+smallimg16[0].onclick=function(){
+    mainImage16.src=smallimg16[0].src;
+}
+
+smallimg16[1].onclick=function(){
+    mainImage16.src=smallimg16[1].src;
+}
+
+smallimg16[2].onclick=function(){
+    mainImage16.src=smallimg16[2].src;
+}
+
+// 10th row
+// p-1
+var mainImage17=document.getElementById("mainImage17");
+var smallimg17=document.getElementsByClassName("small-img17");
+
+smallimg17[0].onclick=function(){
+    mainImage17.src=smallimg17[0].src;
+}
+
+smallimg17[1].onclick=function(){
+    mainImage17.src=smallimg17[1].src;
+}
+
+
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if (bar) {
+  bar.addEventListener('click', () => {
+    nav.classList.add('active');
+  });
+}
+
+if (close) {
+  close.addEventListener('click', () => {
+    nav.classList.remove('active');
+  });
+}
+
+
+
+// cart
+
+document.addEventListener("DOMContentLoaded", function () {
+  const cartButtons = document.querySelectorAll(".add-to-cart");
+
+  cartButtons.forEach(button => {
+    button.addEventListener("click", function () {
+      const id = this.dataset.id;
+      const name = this.dataset.name;
+      const price = this.dataset.price;
+      const img = this.dataset.img;
+
+      
+      let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+      
+      const existingItem = cart.find(item => item.id === id);
+      if (existingItem) {
+        existingItem.quantity += 1; 
+      } else {
+        cart.push({ id, name, price, img, quantity: 1 });
+      }
+
+      
+      localStorage.setItem("cart", JSON.stringify(cart));
+
+      
+      alert(`${name} has been added to your cart!`);
+
+       document.addEventListener("DOMContentLoaded", function () {
+    const cartButtons = document.querySelectorAll(".add-to-cart");
+
+    
+    cartButtons.forEach(button => {
+      button.addEventListener("click", function () {
+        const id = this.dataset.id;
+        const name = this.dataset.name;
+        const price = parseFloat(this.dataset.price);
+        const img = this.dataset.img;
+
+        let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+        const existingItem = cart.find(item => item.id === id);
+        if (existingItem) {
+          existingItem.quantity += 1;
+        } else {
+          cart.push({ id, name, price, img, quantity: 1 });
+        }
+
+        localStorage.setItem("cart", JSON.stringify(cart));
+        updateCartCount();
+        alert(`${name} added to your cart!`);
+      });
+    });
+
+    
+  });
+    });
+  });
+});
+
+function updateCartCount() {
+  const countEl = document.getElementById("cart-count");
+  if (!countEl) return;
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+  let totalCount = cart.reduce((sum, item) => sum + item.quantity, 0);
+  countEl.textContent = totalCount;
+}
+
+
+document.addEventListener("DOMContentLoaded", updateCartCount);
+
+window.addEventListener("storage", updateCartCount);
+
+localStorage.setItem("cart", JSON.stringify(cart));
+updateCartCount();
+
---
+++ refrig.js
@@ -0,0 +1,339 @@
+// row-1
+// p-1
+var mainImage =document.getElementById("mainImg");
+var smallimg=document.getElementsByClassName("small-img");
+
+smallimg[0].onclick=function(){
+    mainImage.src=smallimg[0].src;
+}
+
+smallimg[1].onclick=function(){
+    mainImage.src=smallimg[1].src;
+}
+
+smallimg[2].onclick=function(){
+    mainImage.src=smallimg[2].src;
+}
+
+// p-2
+var mainImage1 =document.getElementById("mainImage1");
+var smallimg1=document.getElementsByClassName("small-img1");
+
+smallimg1[0].onclick=function(){
+    mainImage1.src=smallimg1[0].src;
+}
+
+smallimg1[1].onclick=function(){
+    mainImage1.src=smallimg1[1].src;
+}
+
+smallimg1[2].onclick=function(){
+    mainImage1.src=smallimg1[2].src;
+}
+
+// p-3
+var mainImage2 =document.getElementById("mainImage2");
+var smallimg2=document.getElementsByClassName("small-img2");
+
+smallimg2[0].onclick=function(){
+    mainImage2.src=smallimg2[0].src;
+}
+
+smallimg2[1].onclick=function(){
+    mainImage2.src=smallimg2[1].src;
+}
+
+smallimg2[2].onclick=function(){
+    mainImage2.src=smallimg2[2].src;
+}
+smallimg2[3].onclick=function(){
+    mainImage2.src=smallimg2[3].src;
+}
+
+// row-2
+// p-1
+var mainImage3=document.getElementById("mainImage3");
+var smallimg3=document.getElementsByClassName("small-img3");
+
+smallimg3[0].onclick=function(){
+    mainImage3.src=smallimg3[0].src;
+}
+
+smallimg3[1].onclick=function(){
+    mainImage3.src=smallimg3[1].src;
+}
+
+smallimg3[2].onclick=function(){
+    mainImage3.src=smallimg3[2].src;
+}
+
+// p-2
+var mainImage4=document.getElementById("mainImage4");
+var smallimg4=document.getElementsByClassName("small-img4");
+
+smallimg4[0].onclick=function(){
+    mainImage4.src=smallimg4[0].src;
+}
+
+smallimg4[1].onclick=function(){
+    mainImage4.src=smallimg4[1].src;
+}
+
+smallimg4[2].onclick=function(){
+    mainImage4.src=smallimg4[2].src;
+}
+
+// p-3
+var mainImage5=document.getElementById("mainImage5");
+var smallimg5=document.getElementsByClassName("small-img5");
+
+smallimg5[0].onclick=function(){
+    mainImage5.src=smallimg5[0].src;
+}
+
+smallimg5[1].onclick=function(){
+    mainImage5.src=smallimg5[1].src;
+}
+
+smallimg5[2].onclick=function(){
+    mainImage5.src=smallimg5[2].src;
+}
+
+// row-3
+// p-1
+
+var mainImage6=document.getElementById("mainImage6");
+var smallimg6=document.getElementsByClassName("small-img6");
+
+smallimg6[0].onclick=function(){
+    mainImage6.src=smallimg6[0].src;
+}
+
+smallimg6[1].onclick=function(){
+    mainImage6.src=smallimg6[1].src;
+}
+
+smallimg6[2].onclick=function(){
+    mainImage6.src=smallimg6[2].src;
+}
+
+// p-2
+var mainImage7=document.getElementById("mainImage7");
+var smallimg7=document.getElementsByClassName("small-img7");
+
+smallimg7[0].onclick=function(){
+    mainImage7.src=smallimg7[0].src;
+}
+
+smallimg7[1].onclick=function(){
+    mainImage7.src=smallimg7[1].src;
+}
+
+
+
+// p-3
+var mainImage8=document.getElementById("mainImage8");
+var smallimg8=document.getElementsByClassName("small-img8");
+
+smallimg8[0].onclick=function(){
+    mainImage8.src=smallimg8[0].src;
+}
+
+smallimg8[1].onclick=function(){
+    mainImage8.src=smallimg8[1].src;
+}
+
+smallimg8[2].onclick=function(){
+    mainImage8.src=smallimg8[2].src;
+}
+
+// row-4
+// p-1
+var mainImage9=document.getElementById("mainImage9");
+var smallimg9=document.getElementsByClassName("small-img9");
+
+smallimg9[0].onclick=function(){
+    mainImage9.src=smallimg9[0].src;
+}
+
+smallimg9[1].onclick=function(){
+    mainImage9.src=smallimg9[1].src;
+}
+
+// p-2
+var mainImage10=document.getElementById("mainImage10");
+var smallimg10=document.getElementsByClassName("small-img10");
+
+smallimg10[0].onclick=function(){
+    mainImage10.src=smallimg10[0].src;
+}
+
+smallimg10[1].onclick=function(){
+    mainImage10.src=smallimg10[1].src;
+}
+
+smallimg10[2].onclick=function(){
+    mainImage10.src=smallimg10[2].src;
+}
+
+// p-3
+var mainImage11=document.getElementById("mainImage11");
+var smallimg11=document.getElementsByClassName("small-img11");
+
+smallimg11[0].onclick=function(){
+    mainImage11.src=smallimg11[0].src;
+}
+
+smallimg11[1].onclick=function(){
+    mainImage11.src=smallimg11[1].src;
+}
+
+smallimg11[2].onclick=function(){
+    mainImage11.src=smallimg11[2].src;
+}
+
+// 5th row
+// p-1
+var mainImage12=document.getElementById("mainImage12");
+var smallimg12=document.getElementsByClassName("small-img12");
+
+smallimg12[0].onclick=function(){
+    mainImage12.src=smallimg12[0].src;
+}
+
+smallimg12[1].onclick=function(){
+    mainImage12.src=smallimg12[1].src;
+}
+
+smallimg12[2].onclick=function(){
+    mainImage12.src=smallimg12[2].src;
+}
+
+// p-2
+var mainImage13=document.getElementById("mainImage13");
+var smallimg13=document.getElementsByClassName("small-img13");
+
+smallimg13[0].onclick=function(){
+    mainImage13.src=smallimg13[0].src;
+}
+
+smallimg13[1].onclick=function(){
+    mainImage13.src=smallimg13[1].src;
+}
+
+smallimg13[2].onclick=function(){
+    mainImage13.src=smallimg13[2].src;
+}
+
+// p-3
+var mainImage14=document.getElementById("mainImage14");
+var smallimg14=document.getElementsByClassName("small-img14");
+
+smallimg14[0].onclick=function(){
+    mainImage14.src=smallimg14[0].src;
+}
+
+smallimg14[1].onclick=function(){
+    mainImage14.src=smallimg14[1].src;
+}
+
+smallimg14[2].onclick=function(){
+    mainImage14.src=smallimg14[2].src;
+}
+
+
+
+
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if (bar) {
+  bar.addEventListener('click', () => {
+    nav.classList.add('active');
+  });
+}
+
+if (close) {
+  close.addEventListener('click', () => {
+    nav.classList.remove('active');
+  });
+}
+
+// cart
+
+document.addEventListener("DOMContentLoaded", function () {
+  const cartButtons = document.querySelectorAll(".add-to-cart");
+
+  cartButtons.forEach(button => {
+    button.addEventListener("click", function () {
+      const id = this.dataset.id;
+      const name = this.dataset.name;
+      const price = this.dataset.price;
+      const img = this.dataset.img;
+
+      // Get existing cart from localStorage or create new one
+      let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+      // Check if product already exists
+      const existingItem = cart.find(item => item.id === id);
+      if (existingItem) {
+        existingItem.quantity += 1; // Increase quantity if already in cart
+      } else {
+        cart.push({ id, name, price, img, quantity: 1 });
+      }
+
+      // Save back to localStorage
+      localStorage.setItem("cart", JSON.stringify(cart));
+
+      // Confirmation alert
+      alert(`${name} has been added to your cart!`);
+
+       document.addEventListener("DOMContentLoaded", function () {
+    const cartButtons = document.querySelectorAll(".add-to-cart");
+
+    // Add item to localStorage cart
+    cartButtons.forEach(button => {
+      button.addEventListener("click", function () {
+        const id = this.dataset.id;
+        const name = this.dataset.name;
+        const price = parseFloat(this.dataset.price);
+        const img = this.dataset.img;
+
+        let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+        const existingItem = cart.find(item => item.id === id);
+        if (existingItem) {
+          existingItem.quantity += 1;
+        } else {
+          cart.push({ id, name, price, img, quantity: 1 });
+        }
+
+        localStorage.setItem("cart", JSON.stringify(cart));
+        updateCartCount();
+        alert(`${name} added to your cart!`);
+      });
+    });
+
+    
+  });
+    });
+  });
+});
+
+function updateCartCount() {
+  const countEl = document.getElementById("cart-count");
+  if (!countEl) return;
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+  let totalCount = cart.reduce((sum, item) => sum + item.quantity, 0);
+  countEl.textContent = totalCount;
+}
+
+// Update count on page load
+document.addEventListener("DOMContentLoaded", updateCartCount);
+
+// Recalculate whenever cart changes (optional trigger for add-to-cart buttons)
+window.addEventListener("storage", updateCartCount);
+
+localStorage.setItem("cart", JSON.stringify(cart));
+updateCartCount();
